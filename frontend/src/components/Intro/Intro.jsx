b63960365d5b5bef722d0665677be3855317b326
Artem Zubov <artmzbv@gmail.com>
2024-12-21 16:56:12 +0300

    intro

--- frontend/src/components/Intro/Intro.jsx
+++ frontend/src/components/Intro/Intro.jsx
@@ -1,24 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Intro.css'; 
 import Button from '../common/Button/Button'
+import intro from '../../images/intro/webp-test-mobile_.webp'
 
 const Intro = () => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 800);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setIsMobile(window.innerWidth < 800);
+      };
+  
+      // Add event listener for window resize
+      window.addEventListener('resize', handleResize);
+  
+      // Cleanup the event listener on component unmount
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+  
     return (
-        <section className='intro' id='intro'>
-            <div className='intro__container'>
-            <div className='intro__text'>
+      <section className='intro' id='intro'>
+        <div className='intro__container'>
+          <div className='intro__text'>
             <h1 className='intro__title'>{`It’s all about you —\nyour story, your vibe. Just you!`}</h1>
-            <p className='intro__subtitle'>30£ for 15 min</p>
-            <p className='intro__subtitle'>40£ for 30 min</p>
-            <p className='intro__subtitle'>60£ for 45 min</p>
-            <p className='intro__subtitle'>70£ for 60 min</p>
-            </div>
-            <div className='intro__buttons'>
-            <Button book={true} type={"time"} value={"BOOK"}/>
-            <Button book={false} type={"gift"} value={"BUY AS A GIFT"}/>
-            </div>
-            </div>
-        </section>
+            
+            {/* Render for screens larger than 500px */}
+            {!isMobile && (
+              <>
+                <ul className='intro__list'>
+                <li className='intro__subtitle'>30£ for 15 min</li>
+                <li className='intro__subtitle'>40£ for 30 min</li>
+                <li className='intro__subtitle'>60£ for 45 min</li>
+                <li className='intro__subtitle'>70£ for 60 min</li>
+                </ul>
+                <div className='intro__buttons'>
+                <Button book={true} type={"time"} value={"BOOK"} />
+                <Button book={false} type={"gift"} value={"BUY AS A GIFT"} />
+                </div>
+              </>
+            )}
+  
+            {/* Render for screens smaller than 500px */}
+            {isMobile && (
+              <div className='intro__mobile-container'>
+                <ul className='intro__list'>
+                <li className='intro__subtitle'>30£ for 15 min</li>
+                <li className='intro__subtitle'>40£ for 30 min</li>
+                <li className='intro__subtitle'>60£ for 45 min</li>
+                <li className='intro__subtitle'>70£ for 60 min</li>
+                </ul>
+                <div className='intro__buttons'>
+                <Button book={true} type={"time"} value={"BOOK"} />
+                <Button book={false} type={"gift"} value={"BUY AS A GIFT"} />
+                </div>
+              </div>
+            )}
+          </div>
+          <img src={intro} className='intro__picture' alt="Intro" />
+        </div>
+      </section>
     );
   };
   

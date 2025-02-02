755bdd179f5b498a039d85a0a2a4ceec33903e80
Artem Zubov <artmzbv@gmail.com>
2025-02-02 23:19:46 +0300

    privacy

--- frontend/src/components/Privacy/Privacy.jsx
+++ frontend/src/components/Privacy/Privacy.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import './Privacy.css'; 
-import door from '../../images/privacy/door.png'
-import comfort from '../../images/privacy/comfort.png'
+import quality from '../../images/privacy/quality.webp'
+import comfort from '../../images/privacy/comfort.webp'
 import safety from '../../images/privacy/private.jpg'
 import assurance from '../../images/privacy/assurance.png'
 
 const Privacy = () => {
 
   const options = [
-    {title:'Secure Setting', src: door, 
-    description: 'Away from prying eyes, relax and reflect in our studio space. No third party intervention, just you and your creativity.', alt: 'ready'},
+    {title:'Quality', src: quality, 
+    description: 'Professional camera and lighting set up for high quality photography.', alt: 'ready'},
     {title:'Comfort', src: comfort,
-    description: 'Strike a pose to your favourite playlist or relax to a rhythmical, technical symphony while remotely conducting the studio.', alt: 'cheese'},
+    description: 'Relax in reception before your photoshoot.', alt: 'cheese'},
     {title:'Safety', src: safety, 
-    description: 'Your secure download link ensures that only you can access your photos.' ,alt: 'click'},
+    description: 'Only you can download your images.' ,alt: 'click'},
     {title:'Assurance', src: assurance,
-      description: `Your photos will be deleted automatically after 48 hours.`, alt: 'repeat'},
+      description: `Your photos will be automatically deleted after one week, or sooner if you prefer.`, alt: 'repeat'},
 ]
 
     return (
       <section className='privacy' id="about">
-          <h1 className='privacy__title'>WE VALUE YOUR PRIVACY</h1>
-          <h2 className='privacy__subtitle'>For us, comfort is the foundation of every photoshoot experience</h2>
+          <h1 className='privacy__title'>WHAT WE OFFER</h1>
+          <h2 className='privacy__subtitle'>Everything you need for a successful photoshoot!</h2>
             <div className='privacy__container'>
             {options.map((option) => (
               <div key={option.title} 

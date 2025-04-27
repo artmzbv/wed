b6ebbc610785a05582ceb00bf2aadbe722299394
Artem Zubov <artmzbv@gmail.com>
2025-04-27 18:27:02 +0300

    contactus

--- frontend/src/components/ContactUs/ContactUs.jsx
+++ frontend/src/components/ContactUs/ContactUs.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import './ContactUs.css'; 
+import chamber from '../../images/chamber.png'
 
 const ContactUs = () => {
     return (
       <section className='contact' id='contact'>
         {/* <h1 className='contact__title'>Contact Us</h1> */}
         <div className='contact__container'>
+        <h2 className='contact__title contact__title_mobile'>HOW TO FIND US ?</h2>
           <div className='contact__info'>
-          <h2 className='contact__title'>HOW TO FIND US ?</h2>
-          <p className='contact__adress'>Room 8, 95 Ditchling Road, Brighton, BN1 4ST</p>
+          <div className='contact__adress-container'>
+          <h2 className='contact__title contact__title_desktop'>HOW TO FIND US ?</h2>
+          <p className='contact__adress'>Studio 8, Citibase Brighton, 95 Ditchling Road, Brighton, BN1 4ST</p>
           <p className='contact__adress'>info@self-made-portraits.com</p>
           <p className='contact__adress'>01273 011626</p>
+          </div>
+          <img className='contact__image' src={chamber} ></img>
           {/* <p className='contact__adress'>36</p> */}
           </div>
           <div className='contact__maps'>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10080.296673328663!2d-0.1349094!3d50.8297901!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4875859ce1638ec9%3A0x4d9020301f8d9fd3!2s95%20Shaftesbury%20Rd%2C%20Ditchling%20Rd%2C%20Brighton%20and%20Hove%2C%20Brighton%20BN1%204ST%2C%20UK!5e0!3m2!1sen!2sfr!4v1738317521641!5m2!1sen!2sfr" 
+          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1259.9432515835967!2d-0.13435557976227308!3d50.833266164177566!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4875854f742456e5%3A0xb253b4493ce9f45c!2sSelf-Made%20Portraits!5e0!3m2!1sru!2sru!4v1745599108720!5m2!1sen!2sen" 
           width="100%" 
           height="100%" 
           style={{border: 0}} 

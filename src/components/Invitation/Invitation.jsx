7fafa730d895b3cbca1b95ee8870fd375b496e02
Artem Zubov <artmzbv@gmail.com>
2026-06-30 02:04:35 +0300

    wedding

--- src/components/Invitation/Invitation.jsx
+++ src/components/Invitation/Invitation.jsx
@@ -5,7 +5,7 @@ const Invitation = () => {
     <section className="invitation">
       <div className="invitation__overlay" />
       <p className="invitation__text">
-        {`С любовью и благодарностью приглашаем вас разделить радость\n нашего свадебного дня.`}
+        {`С любовью и благодарностью приглашаем вас разделить радость\n нашего свадебного дня`}
       </p>
     </section>
   );
